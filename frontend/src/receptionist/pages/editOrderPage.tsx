dd37c188736f33a60a2fff0bd4ced79f48f9dc06
Karolina Mačiulevičiūtė <karolina.maciuleviciute@mif.stud.vu.lt>
2025-12-15 20:42:55 +0200

    Currency context fix

--- frontend/src/receptionist/pages/editOrderPage.tsx
+++ frontend/src/receptionist/pages/editOrderPage.tsx
@@ -3,7 +3,6 @@ import { Suspense } from 'react';
 import SidebarCashier from '@/receptionist/components/sidebarCashier';
 import Topbar from '@/global/components/topbar';
 import { CartItem, CartProvider } from '@/receptionist/contexts/cartContext';
-import { CurrencyProvider } from '@/global/contexts/currencyContext';
 import ProductGrid from '../components/productGrid';
 import OrderSummary from '../components/orderSummary';
 import useSWR from 'swr';
@@ -31,32 +30,28 @@ function OrderPanel() {
   const { authFetchJson } = useAuth();
   const { data: items } = useSWR(
     `order/${params.orderId}`,
-    (url) => authFetchJson<CartItem[]>(url, "GET"),
+    url => authFetchJson<CartItem[]>(url, 'GET'),
     {
       suspense: true,
       revalidateOnMount: true,
-    }
+    },
   );
 
   return (
-    <CurrencyProvider>
-      <CartProvider initItems={items}>
-        <main className="flex-1 overflow-y-auto">
-          <div className="flex gap-6 p-6">
-
-            {/* Left: Products */}
-            <div className="flex-1">
-              <ProductGrid />
-            </div>
-
-            {/* Right: Order summary */}
-            <div className="w-1/3 max-w-md">
-              <OrderSummary showPaymentSection={true}/>
-            </div>
-
+    <CartProvider initItems={items}>
+      <main className="flex-1 overflow-y-auto">
+        <div className="flex gap-6 p-6">
+          {/* Left: Products */}
+          <div className="flex-1">
+            <ProductGrid />
           </div>
-        </main>
-      </CartProvider>
-    </CurrencyProvider>
+
+          {/* Right: Order summary */}
+          <div className="w-1/3 max-w-md">
+            <OrderSummary showPaymentSection={true} />
+          </div>
+        </div>
+      </main>
+    </CartProvider>
   );
 }
